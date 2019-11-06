05037d0cb4a28ffbd9a4da7922b47541efc75a05
wku <wku@naver.com>
2019-11-06 09:39:18 +0900

    create num

--- 1189/src/hello.java
+++ 1189/src/hello.java
@@ -3,5 +3,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("computer SW");
 		System.out.println("wonkawang Univ");
+		System.out.println("20131189");
 		}
 }
