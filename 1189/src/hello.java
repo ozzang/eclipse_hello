8b7507cc2179dc606f6eb788331eb0f48d75d5d8
wku <wku@naver.com>
2019-11-06 09:38:58 +0900

    create name

--- 1189/src/hello.java
+++ 1189/src/hello.java
@@ -3,5 +3,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("computer SW");
 		System.out.println("wonkawang Univ");
+		System.out.println("ø¿√¢ø≠");
 		}
 }
