700a6ab53422dc45743e72540365f620db0a87db
wku <wku@naver.com>
2019-11-14 15:05:02 +0900

    add line4

--- 1189/src/PushPrint.java
+++ 1189/src/PushPrint.java
@@ -3,5 +3,6 @@ public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class");
 		System.out.println("Add Java Class2");
+		System.out.println("Add Java Class4");
 		}
 }
